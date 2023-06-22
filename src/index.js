149a6475e2fed3084a1b3f974b5feb9359822cdf
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-06-22 14:20:59 +0300

    tasklistener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById(`create-task-form`)
+  const ulElement=document.getElementById(`task`)
+  form.addEventListener(`submit`, (e)=>{
+    e.preventDefault();
+    const input = document.getElementById('new-task-description');
+    const taskDescription = input.value;gir
+    const liElement = document.createElement('li');
+    liElement.textContent = taskDescription;
+    ulElement.appendChild(liElement);
+    input.value = '';
+  });
 });
